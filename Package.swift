b344371b39fd78150eea5322b486d0812f6fb937
Andrea Scuderi <andreascuderi@ymail.com>
2020-03-09 10:42:22 +0000

    Add connection pooling from async-http-client master branch

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         //.package(path: "../aws-lambda-swift-sprinter-core/"),
         .package(url: "https://github.com/swift-sprinter/aws-lambda-swift-sprinter-core", from: "1.0.0"),
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.0"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", .branch("master")),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.8.0"),
     ],
     targets: [
